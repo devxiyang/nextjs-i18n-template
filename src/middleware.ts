8e6e3f8e8f631c3a447373c59401cb3e959a4f8a
devxiyang <devxiyang@gmail.com>
2025-03-19 21:34:19 +0800

    init

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
-// import { siteConfig } from './config/site.config';
  
 export default createMiddleware(routing);
  
-// export const config = {
-//   // Match only internationalized pathnames
-//   matcher: ['/', `/(${siteConfig.locales.join('|')})/:path*`]
-// };
+export const config = {
+  // Match only internationalized pathnames
+  matcher: ['/((?!_next|api|.*\\..*).*)']
+};
