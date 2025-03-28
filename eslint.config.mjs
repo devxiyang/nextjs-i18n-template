b6689251aa8323d2e519c1e22891d63a9bc59f83
devxiyang <devxiyang@gmail.com>
2025-03-28 08:56:18 +0800

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,9 +14,9 @@ const eslintConfig = [
   {
     rules: {
       // 放松React Hook缺失依赖规则
-      "react-hooks/exhaustive-deps": "warn",
+      // "react-hooks/exhaustive-deps": "warn",
       // 允许使用any类型
-      "@typescript-eslint/no-explicit-any": "warn",
+      // "@typescript-eslint/no-explicit-any": "warn",
       // 放松未使用变量规则
       "@typescript-eslint/no-unused-vars": ["warn", { 
         "varsIgnorePattern": "^_", 
